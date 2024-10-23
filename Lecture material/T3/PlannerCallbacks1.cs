eb18732bf667659b48cf8606304d2cc5a78cf1e9
Ejbejaranos <91475734+EjbejaranosAI@users.noreply.github.com>
2024-10-24 01:40:54 +0200

    Create PlannerCallbacks1.cs

---
+++ Lecture material/T3/PlannerCallbacks1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlannerCallbacks : MonoBehaviour
+{
+    Moves moves;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    void Start()
+    {
+        moves = this.GetComponent<Moves>();
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    public void Steal(GameObject treasure)
+    {
+        treasure.GetComponent<Renderer>().enabled = false;
+    }
+
+    public IEnumerator Seek(GameObject treasure, GameObject copGO)
+    {
+        agent.SetDestination(treasure.transform.position);
+        while ((Vector3.Distance(treasure.transform.position, transform.position) > 2f) &&
+               (Vector3.Distance(treasure.transform.position, copGO.transform.position) > 10f))
+            yield return null;
+    }
+
+    public IEnumerator Wander(GameObject cop, GameObject treasure)
+    {
+        while (Vector3.Distance(treasure.transform.position, cop.transform.position) < 10f)
+        {
+            moves.Wander();
+            yield return null;
+        }
+    }    
+}
